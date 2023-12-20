518e1a9b32cf515f3935afbca2395efd7ff6ad93
chvgf <chvgf@naver.com>
2023-12-20 17:51:55 +0900

    a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './css/global';
 import Main from './pages/Main';
 import Community from './pages/Community';
+import Toktok from './components/commnunity/Toktok';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main />} >
           <Route path='/' element={<Community />} />
+          <Route path='/community/Toktok' element={<Toktok />} />
           {/* 쇼핑 컴포넌트 넣어주세요! */}
           {/* <Route path='/shop' element={< />} /> */}
         </Route>
---
+++ src/components/commnunity/Toktok.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Toktok(props) {
+  return (
+    <div className='div'>
+      test
+    </div>
+  );
+}
+
+export default Toktok;
