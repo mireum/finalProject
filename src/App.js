67dcfdb3f07888848ddbcc1706d6ea7589ad6168
fkdkskdfjfk <yljw225@naver.com>
2023-12-20 16:44:19 +0900

    stash

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './css/global';
+import Shop from './pages/Shop';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Routes>
-        <Route path='/' element={}/>
+        <Route path='/' element={<Shop/>}/>
       </Routes>
     </>
   );
---
+++ src/components/shop/Shopslide.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Shopslide(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Shopslide;
