f78e295d0f7b2979190f3e5260f0c59564a6b955
fkdkskdfjfk <yljw225@naver.com>
2023-12-21 14:32:11 +0900

    슬라완

--- src/components/shop/Shopslide.js
+++ src/components/shop/Shopslide.js
@@ -1,48 +1,8 @@
 import React from 'react';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"
-import styled from 'styled-components';
+import { StyledSlide } from "../../css/Shop";
 import img1 from "../../image/1.png";
 
-const StyledSlide = styled(Slider)`
-  .slick-list {
-    position: relative;
-    width: 1200px;
-    height: 400px;
-    margin: 0 auto;
-    background-color: #cccccc;
-    /* overflow: hidden; */
-  }
-  .slick-list:hover {
-    .slick-prev {
-      opacity: 1;
-    }
-  }
-  
-  .slick-prev, .slick-next {
-    position: absolute;
-    z-index: 1;
-  }
-  .slick-prev {
-    left: 470px;
-  }
-  .slick-next {
-    right: 490px
-  }
-  /* .slick-prev:hover {
-    opacity: 1;
-  } */
 
-  .slick-prev:before, .slick-next:before{ 
-    font-family: 'slick';
-    font-size: 40px;
-    line-height: 1;
-    opacity: .75;
-    color: #c9bbbb;
-    -webkit-font-smoothing: antialiased;
-  }  
-`;
 
 function Shopslide(props) {
 
